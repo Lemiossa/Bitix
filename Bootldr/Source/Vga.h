c977f73d2414f0320dcca15fbaa1eb516b673308
Matheus Leme Da Silva <matheuslemedasilva000@gmail.com>
2026-02-19 00:05:04 -0300

    Vga.c separado e corrigido de > para >= em limites do x e y

---
+++ Bootldr/Source/Vga.h
@@ -0,0 +1,18 @@
+/************************************
+ * Vga.h                            *
+ * Criado por Matheus Leme Da Silva *
+ ***********************************/
+#ifndef VGA_H
+#define VGA_H
+#include "Types.h"
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+void vga_set_cursor(uint16_t x, uint16_t y);
+void vga_put_char(uint16_t x, uint16_t y, char c, uint8_t attributes);
+uint16_t vga_get_char(uint16_t x, uint16_t y);
+void vga_scroll(void);
+void vga_clear(void);
+
+#endif /* VGA_H */
