cb8ce15fca30fce576201f5539a667204c2ded41
Matheus Leme Da Silva <matheuslemedasilva000@gmail.com>
2026-03-05 11:41:55 -0300

    Kernel: separei cpuid corretamente

---
+++ kernel/source/arch/x86/cpuid.c
@@ -0,0 +1,25 @@
+/************************************
+ * cpuid.c                          *
+ * Criado por Matheus Leme Da Silva *
+ ***********************************/
+#include <stdint.h>
+#include <stddef.h>
+#include <asm.h>
+
+char cpu_vendor[13] = {0};
+uint32_t cpu_features_ebx = 0;
+uint32_t cpu_features_ecx = 0;
+uint32_t cpu_features_edx = 0;
+
+/* Pega todas as features da CPU e coloca na variavel global CPU features ebx, ecx e edx*/
+void cpuid_get_features(void)
+{
+	uint32_t ebx = 0, ecx = 0, edx = 0;
+	cpuid(0, NULL, &ebx, &ecx, &edx);
+
+	((uint32_t *)cpu_vendor)[0] = ebx;
+	((uint32_t *)cpu_vendor)[1] = edx;
+	((uint32_t *)cpu_vendor)[2] = ecx;
+	cpuid(1, NULL, &cpu_features_ebx, &cpu_features_ecx, &cpu_features_edx);
+}
+
