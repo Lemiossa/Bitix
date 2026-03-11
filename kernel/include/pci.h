e4b84709959efececeffcf1c7973a3bd73af964e
Matheus Leme Da Silva <matheuslemedasilva000@gmail.com>
2026-03-11 10:03:44 -0300

    Kernel: Aumentei de 64 dispositivos PCI para 256

--- kernel/include/pci.h
+++ kernel/include/pci.h
@@ -18,7 +18,7 @@ typedef struct pci_device {
 	uint32_t bars[6];
 } pci_device_t;
 
-#define MAX_PCI_DEVICES 64
+#define MAX_PCI_DEVICES 256
 
 uint32_t pci_read(uint8_t bus, uint8_t dev, uint8_t func, uint8_t reg);
 pci_device_t *pci_find(uint8_t class, uint8_t subclass);
